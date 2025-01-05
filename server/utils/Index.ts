b1337342ccc24d63af195bb2c6dcc95582aad4f4
Kristan Schlikow <kristian@schlikow.de>
2025-01-05 21:23:31 +0100

    Update packages, change exports for server side

--- server/utils/Index.ts
+++ server/utils/Index.ts
@@ -1,4 +1,14 @@
-export * from '~/utils/drizzle/schema';
-export * from '~/utils/drizzle/relations';
-export type * from '~/utils/Interfaces';
+export { characters, definitionHistory, ratings, tags, definitions, lorebooks, users, relations, definitionTags, tagCounts } from '~/utils/drizzle/schema';
+export { charactersRelations, usersRelations, definitionHistoryRelations, definitionsRelations, ratingsRelations, relationsRelations } from '~/utils/drizzle/relations';
+export { Character, RemoteChar, Rating, Definition, DefinitionRow, FileRow, FileUpload, LogEntry, Statistics, StatisticsCache, Relation, User } from '~/utils/Interfaces';
+export { ComparisonOperator, SearchFlags } from '~/utils/Search';
+export type {
+    ComparisonOperatorSymbol,
+    SearchFlagString,
+    SearchFlagEmbedding,
+    SearchFlagNumber,
+    SearchFlagNumberRange,
+    SearchFlagNumberOrRange,
+    SearchFlagStringOrEmbedding,
+} from '~/utils/Search';
 export { StatusCode } from '~/utils/StatusCode';
