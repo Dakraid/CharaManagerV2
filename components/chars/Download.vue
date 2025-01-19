f5532b2a26a0dc2ac9a849c408c30bde20cf5fe5
Kristan Schlikow <kristian@schlikow.de>
2025-01-19 02:03:14 +0100

    Adjust transition for fetching remote characters

--- components/chars/Download.vue
+++ components/chars/Download.vue
@@ -223,10 +223,7 @@ onMounted(async () => {
 
         <Transition>
             <LoaderCircle v-if="downloading" class="h-20 w-20 mx-auto motion-safe:animate-spin" />
-        </Transition>
-
-        <Transition>
-            <div v-if="files.length !== 0" class="flex flex-col gap-4">
+            <div v-else-if="files.length !== 0 && !downloading" class="flex flex-col gap-4">
                 <Button variant="outline" @click="uploadFiles">Save</Button>
                 <div class="flex items-top justify-center gap-2">
                     <Checkbox id="importNotes" v-model:checked="personalityToCreatorNotes" />
@@ -238,11 +235,8 @@ onMounted(async () => {
                     </div>
                 </div>
             </div>
-        </Transition>
-
-        <Transition>
             <div
-                v-if="files.length !== 0 && files.length <= 10"
+                v-if="files.length !== 0 && files.length <= 10 && !downloading"
                 id="previewContainerUpload"
                 class="flex flex-wrap overflow-y-scroll bg-card rounded-md border p-4"
                 :style="`max-height: ${maxHeight}px;`">
@@ -250,7 +244,7 @@ onMounted(async () => {
                     <CharsDisplayUpload :content="file.content" @remove="removeFile(file.name)" />
                 </div>
             </div>
-            <p v-else-if="files.length !== 0" class="text-sm text-center text-muted-foreground">Too many files selected for preview.</p>
+            <p v-else-if="files.length !== 0 && !downloading" class="text-sm text-center text-muted-foreground">Too many files selected for preview.</p>
         </Transition>
     </div>
 </template>
